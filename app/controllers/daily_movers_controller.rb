b3d47ac54f1e661a6939948214c46ca981fb81db
vedad <v.virani95@gmail.com>
2020-11-04 15:43:08 -0800

    heroku push fix

--- app/controllers/daily_movers_controller.rb
+++ app/controllers/daily_movers_controller.rb
@@ -1,6 +1,6 @@
 class DailyMoversController < ApplicationController
   def index
-    @indice = indice_call(['spy','dia','rut'])
+    @indice = indice_call(['spy','dia','iwm'])
     @movers = movers_call()
     render 'index'
   end
